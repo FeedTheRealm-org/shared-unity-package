1fe20f8102165e5991c5407b42b5100b1a4ca1da
maxogod <maxi18wars@gmail.com>
2026-04-06 19:13:30 -0300

    feat: add more character scripts from hero package 4d into shared package

---
+++ Runtime/Core/Systems/Characters/H4DScripts/ColorMode.cs
@@ -0,0 +1,11 @@
+﻿namespace Assets.HeroEditor4D.Common.Scripts.SimpleColorPicker.Scripts
+{
+    /// <summary>
+    /// Color mode.
+    /// </summary>
+    public enum ColorMode
+    {
+        Rgb,
+        Hsv,
+    }
+}
