bcd064def117a269e0d9662d74a5ec2140ab8d64
sev <santisevitz@hotmail.com>
2026-04-14 19:56:06 -0300

    fix: revert aditional fields that are not needed

--- Runtime/Models/Creatables/PortalData.cs
+++ Runtime/Models/Creatables/PortalData.cs
@@ -11,11 +11,6 @@ namespace FTRShared.Runtime.Models
         public int zoneId;
         public string targetPortalId;
 
-        // Here we save the name of the target portal and the position to teleport to,
-        // so we don't have to do an extra lookup when accepting the portal request.
-        public string targetPortalName;
-        public Vector3 targetPosition;
-
         public PortalData(string id, string name, int zoneId)
         {
             this.id = id;
