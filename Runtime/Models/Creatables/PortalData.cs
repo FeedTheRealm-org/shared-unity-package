5b171cd5d3ba45e0e1acc17c95fd19c0246245a8
sev <santisevitz@hotmail.com>
2026-04-14 15:39:29 -0300

    adding 2 new portal fields to the creatables data to simplify the data

--- Runtime/Models/Creatables/PortalData.cs
+++ Runtime/Models/Creatables/PortalData.cs
@@ -8,8 +8,13 @@ namespace FTRShared.Runtime.Models
     {
         public string id;
         public string name;
-        public string targetPortalId;
         public int zoneId;
+        public string targetPortalId;
+
+        // Here we save the name of the target portal and the position to teleport to,
+        // so we don't have to do an extra lookup when accepting the portal request.
+        public string targetPortalName;
+        public Vector3 targetPosition;
 
         public PortalData(string id, string name, int zoneId)
         {
