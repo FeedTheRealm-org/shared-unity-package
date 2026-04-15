c44b334ca92d72a61c697bb2bbccae3729edcdd3
niko <nrsanchez@fi.uba.ar>
2026-04-15 18:00:49 -0300

    refactor: changed quest decision data to be able to handle same quest id but different npc.

--- Runtime/Models/Creatables/QuestDecisionData.cs
+++ Runtime/Models/Creatables/QuestDecisionData.cs
@@ -4,27 +4,34 @@ namespace FTRShared.Runtime.Models
     {
         public QuestData Quest;
         public uint TargetNetId;
+        public string NpcId;
 
-        public QuestPromptData(QuestData quest, uint targetNetId)
+        public QuestPromptData(QuestData quest, uint targetNetId, string npcId = "")
         {
             Quest = quest;
             TargetNetId = targetNetId;
+            NpcId = npcId;
         }
     }
 
     public class QuestDecisionData
     {
         public QuestData Quest;
-
         public bool IsAccepted;
-
         public uint TargetNetId;
+        public string NpcId;
 
-        public QuestDecisionData(QuestData questData, bool isAccepted, uint targetNetId)
+        public QuestDecisionData(
+            QuestData questData,
+            bool isAccepted,
+            uint targetNetId,
+            string npcId = ""
+        )
         {
             Quest = questData;
             IsAccepted = isAccepted;
             TargetNetId = targetNetId;
+            NpcId = npcId;
         }
     }
 }
