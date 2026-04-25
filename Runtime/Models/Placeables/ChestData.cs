2ff9d705c337b6ae4a6547e9cafaaebef4e705ba
sev <santisevitz@hotmail.com>
2026-04-25 15:00:19 -0300

    feat: adding chest data

---
+++ Runtime/Models/Placeables/ChestData.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+namespace FTRShared.Runtime.Models
+{
+    [Serializable]
+    public class ChestData
+    {
+        public string id;
+        public string name = "Chest";
+        public string lootTableId;
+        public Vector3 size = Vector3.one;
+        public Vector3 rotation = Vector3.zero;
+        public Vector3 position = Vector3.zero;
+        public ChestModelData opendedChestModelData;
+        public ChestModelData closedChestModelData;
+    }
+
+    public class ChestModelData
+    {
+        public string modelId;
+        public Vector3 relativePosition = Vector3.zero;
+        public Vector3 relativeRotation = Vector3.zero;
+        public Vector3 relativeSize = Vector3.one;
+    }
+}
