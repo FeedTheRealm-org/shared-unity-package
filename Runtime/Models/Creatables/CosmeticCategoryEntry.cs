ae6cedc429b9fdd794395d6c001d076535b518b4
niko <nrsanchez@fi.uba.ar>
2026-04-29 15:58:28 -0300

    fix: improved data management on cosmetic data.

---
+++ Runtime/Models/Creatables/CosmeticCategoryEntry.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+namespace FTRShared.Runtime.Models
+{
+    [Serializable]
+    public class CosmeticCategoryEntry
+    {
+        public string sprite_path = "";
+
+        public string url_id = "";
+
+        public float price = 0f;
+
+        public CosmeticCategoryEntry() { }
+
+        public CosmeticCategoryEntry(string spritePath, string urlId = "", float price = 0f)
+        {
+            this.sprite_path = spritePath ?? "";
+            this.url_id = urlId ?? "";
+            this.price = price;
+        }
+    }
+}
